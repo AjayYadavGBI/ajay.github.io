29a329ba0008223bf85a0013fbe4c152f4061869
AjayYadavGBI <ajay_yadav@gbinternational.in>
2019-08-16 17:51:29 +0530

    Added new file

---
+++ learn.php
@@ -0,0 +1,13 @@
+.....
+OPtimize Sleep
+Exercise Daily
+Cold Shower
+Medidate Daily for 30 days
+Optimize your diet
+NO Social Media in morning
+	and wile going to the bed
+Seeds and weeds for learning
+Curate your social media
+Constantly teach ( Explane everything on the white board)
+Take Frequent Breaks ( Takes rest often to make your mind away what you were learning at the time and trying to give them the rest)
+
