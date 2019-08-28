c3138cdec805971f0cb3f21e455a4a008ce28bd2
AjayYadavGBI <ajay_yadav@gbinternational.in>
2019-08-28 10:11:51 +0530

    Added this week task in the list

--- learn.php
+++ learn.php
@@ -11,3 +11,13 @@ Curate your social media
 Constantly teach ( Explane everything on the white board)
 Take Frequent Breaks ( Takes rest often to make your mind away what you were learning at the time and trying to give them the rest)
 
+---------------------------------------------------------------------------------------------------------
+
+Today is 28-08-19
+   = Gols for this week
+	1. Create one ml project from Youtube
+	2. Restart our website .......
+	3. Gathering 10 Ideas which can be source of income 
+
+
+
