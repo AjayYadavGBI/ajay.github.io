02389bf5b728788900486d229a9b1e46afb54bca
AjayYadavGBI <ajay_yadav@gbinternational.in>
2019-08-28 11:33:33 +0530

    added to the learn.php

--- learn.php
+++ learn.php
@@ -18,6 +18,22 @@ Today is 28-08-19
 	1. Create one ml project from Youtube
 	2. Restart our website .......
 	3. Gathering 10 Ideas which can be source of income 
+	4. Create a Video for the youtube
+
+
+............................................................
+
+4. Start Python machine learing videos for the machine learing   on youtube.......
+    
+    and Create some General Questions and answers videos on
+    the computer for the youtube....
+So i have decided two things where i can use it to make youtube video with the help of these two videos......
+	1. Tutorial point
+	2. Android app which can be used to make the video..
+
+
+======================================================
+
 
 
 
